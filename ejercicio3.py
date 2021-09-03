08e673ab9e8eb1a71017eb28834b659035b99da9
axelnerilucero <neriluceroaxel@gmail.com>
2021-09-03 06:23:59 -0500

    Version final ejercicio pares

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,7 +2,6 @@ a = ["1","2","3","4","5","6","7","8","9","10"]
 b = ["1","1","3","5","8","13","21"]
 
 def juntar():
-    for i,x in zip(a,b):
-        print("{"+",".join((a,b))+"}")
-
+    nueva = list(zip(a,b)) #junta dos listas 
+    print(nueva)
 juntar()
