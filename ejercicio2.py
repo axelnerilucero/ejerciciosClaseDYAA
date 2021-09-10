b8bab0d6c5922e954ded5546fc7603d565d3ce2a
axelnerilucero <neriluceroaxel@gmail.com>
2021-09-10 13:26:27 -0500

    Cambios finales

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,12 +2,28 @@ import random
 
 def puntaje(rondas):
     count = 0
-    while count > rondas:
+    puntajes_posibles1 = ["15","30","40", "deuce", "advantage", "ganaste"]
+    puntajes_posibles2 = ["15","30","40", "deuce", "advantage", "ganaste"]
+
+    while count < rondas:# or (puntajes_posibles1[5] == "ganaste" or puntajes_posibles2[5] == "ganaste"):
+        print("ronda"+str(count)+"\n")
         jugador = random.randint(1,2)
         if jugador == 1:
-            print("un punto")
+            print("jugador 1: "+puntajes_posibles1[0]+" --- jugador 2: "+puntajes_posibles2[1])
+            puntajes_posibles1[0] = puntajes_posibles1[1]
+            puntajes_posibles1[1] = puntajes_posibles1[2]
+            puntajes_posibles1[2] = puntajes_posibles1[3]
+            puntajes_posibles1[3] = puntajes_posibles1[4]
+            puntajes_posibles1[4] = puntajes_posibles1[5]
         else:
-            jugador == 2
-            print("un punto")
+            print("jugador 1: "+puntajes_posibles1[0]+" --- jugador 2: "+puntajes_posibles2[2])
+            puntajes_posibles2[0] = puntajes_posibles2[1]
+            puntajes_posibles2[1] = puntajes_posibles2[2]
+            puntajes_posibles2[2] = puntajes_posibles2[3]
+            puntajes_posibles2[3] = puntajes_posibles2[4]
+            puntajes_posibles2[4] = puntajes_posibles2[5]
 
-puntaje(10)
+        count += 1
+
+
+puntaje(13)
