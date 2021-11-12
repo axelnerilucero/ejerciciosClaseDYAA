6d58294800f8b6c34ee59118da1b50e7a4d49b84
axelnerilucero <neriluceroaxel@gmail.com>
2021-11-11 19:03:36 -0600

    Tarea 7, ya muestra las coicidencias que tuvo la palabra y en que posiciones

--- Tarea7/tareaquijote.py
+++ Tarea7/tareaquijote.py
@@ -1,5 +1,6 @@
 f = open('el_quijote_ii.txt','r',encoding="utf8")
 A = f.read()
+resultados = []
 def string_match_brute(A,P):
     count = 0
     for i in range(len(A) - len(P) + 1):
@@ -10,8 +11,10 @@ def string_match_brute(A,P):
                 break
         if j+1 == len(P) and A[i+j] == P[j]:
             count = count + 1
-            print(f"Palabra {P} encontrada en la posicion {i}")
-    print(f"con la palabra {P} se encontraron {count} coincidencias")
+            resultados.append(i)
+
+    print(f"con la palabra {P} se encontraron {count} coincidencias en las siguientes posiciones {resultados}")
+    resultados.clear()
 
 entrada = "0011100101011011101"
 patron = ["molino", "hidalgo", "merced"]
